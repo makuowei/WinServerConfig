542a6bb6a379d99cd2461e3fefeb1ba82952740d
makuowei <makuowei@hotmail.com>
2020-06-04 18:37:25 +0800

    Create WindowsUpdate.ps1

---
+++ WindowsUpdate.ps1
@@ -0,0 +1,12 @@
+# Perform Windows Update using Powershell
+
+# Need to install the Windows Update feature
+Install-Module PSWindowsUpdate
+
+# Get latest Windows Updates
+Get-WindowsUpdate
+
+# Install Updates
+Install Windows-Update
+
+# End
